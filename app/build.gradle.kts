f59c0fe4e090c05cc9a151e22af7d482da0c11c8
Alexis Purslane <alexispurslane@pm.me>
2023-09-02 11:34:49 -0400

    fixed version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 31
         targetSdk = 33
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0-alpha.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
